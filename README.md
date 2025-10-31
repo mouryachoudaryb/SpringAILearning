98bd87bc27dd84b98eb63293f44919ef617d045c
mouryachoudarybollineni <mouryachowdary38@gmail.com>
2025-10-31 03:58:26 +0000

    Adding OpenAI Controller to interact with OpenAI Chat Model

---
+++ README.md
@@ -0,0 +1,20 @@
+🚀 Overview
+
+This project provides hands-on experience using the Spring AI framework to interact with various Large Language Models (LLMs).
+It demonstrates how to configure and call different AI backends through a unified API layer in Spring Boot.
+
+Supported providers include:
+
+🤖 OpenAI (e.g., GPT-4, GPT-3.5)
+
+🧰 Tech Stack
+
+Java 25
+
+Spring Boot 3.5.7
+
+Spring AI 1.0.3
+
+Maven
+
+Optional model APIs (OpenAI, Anthropic, Ollama)
---
+++ src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
@@ -0,0 +1,36 @@
+package com.github.mourya.springailearning.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenAIController {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenAIController.class);
+
+    private OpenAiChatModel openAIChatModel;
+
+    public OpenAIController(OpenAiChatModel openAIChatModel) {
+        this.openAIChatModel = openAIChatModel;
+    }
+
+    @GetMapping("/api/{message}")
+    public ResponseEntity<String> hello(@PathVariable String message) {
+
+        try{
+            String response = openAIChatModel.call(message);
+            return ResponseEntity.ok("Response from OpenAIController : " + response);
+        }catch(Exception e){
+            log.error("Exception occurred while calling OpenAI API: ", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                    .body("Error calling external OpenAI service.");
+        }
+
+    }
+}
