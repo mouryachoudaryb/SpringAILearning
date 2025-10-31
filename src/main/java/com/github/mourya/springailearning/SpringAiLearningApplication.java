c4b0f526c9e1c1f66bee167e1af3ffbc2307b195
Mourya Choudary Bollineni <mouryachoudarybollineni@192.168.1.239>
2025-10-31 02:35:33 +0000

    Initial commit

---
+++ src/main/java/com/github/mourya/springailearning/SpringAiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.github.mourya.springailearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mourya/springailearning/SpringAiLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mourya.springailearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
