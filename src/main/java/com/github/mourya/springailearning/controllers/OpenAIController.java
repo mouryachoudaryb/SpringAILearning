165d8ba58ea9e85f478483e2ffceecb057c23ba3
mouryachoudarybollineni <mouryachowdary38@gmail.com>
2025-10-31 11:47:47 +0000

    Making use of ChatClient instead of ChatModel within the OpenAIController

--- src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
+++ src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
@@ -2,6 +2,7 @@ package com.github.mourya.springailearning.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +15,20 @@ public class OpenAIController {
 
     private static final Logger log = LoggerFactory.getLogger(OpenAIController.class);
 
-    private OpenAiChatModel openAIChatModel;
+    private ChatClient chatClient;
 
     public OpenAIController(OpenAiChatModel openAIChatModel) {
-        this.openAIChatModel = openAIChatModel;
+        this.chatClient = ChatClient.create(openAIChatModel);
     }
 
     @GetMapping("/api/{message}")
-    public ResponseEntity<String> hello(@PathVariable String message) {
+    public ResponseEntity<String> chatAnswer(@PathVariable String message) {
 
         try{
-            String response = openAIChatModel.call(message);
-            return ResponseEntity.ok("Response from OpenAIController : " + response);
+            String response = chatClient.prompt(message)
+                    .call()
+                    .content();
+            return ResponseEntity.ok( response);
         }catch(Exception e){
             log.error("Exception occurred while calling OpenAI API: ", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
