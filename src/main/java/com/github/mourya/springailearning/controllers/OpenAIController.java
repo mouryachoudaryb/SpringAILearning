dd3d05592b35801fa35d74171c1b75ddd668a3aa
mouryachoudarybollineni <mouryachowdary38@gmail.com>
2025-10-31 12:04:10 +0000

    Using ChatResponse and Metadata within the OpenAIController

--- src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
+++ src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
@@ -3,6 +3,7 @@ package com.github.mourya.springailearning.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,17 @@ public class OpenAIController {
     public ResponseEntity<String> chatAnswer(@PathVariable String message) {
 
         try{
-            String response = chatClient.prompt(message)
+
+            ChatResponse chatResponse = chatClient.prompt(message)
                     .call()
-                    .content();
+                    .chatResponse();
+
+            System.out.println(chatResponse.getMetadata().getModel());
+
+            String response = chatResponse.getResult()
+                    .getOutput()
+                    .getText();
+
             return ResponseEntity.ok( response);
         }catch(Exception e){
             log.error("Exception occurred while calling OpenAI API: ", e.getMessage());
