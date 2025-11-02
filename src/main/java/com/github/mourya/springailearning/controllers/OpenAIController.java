dc5e4fe2a95da491b565397281dad8b330ecc4e4
mouryachoudarybollineni <mouryachowdary38@gmail.com>
2025-11-02 16:23:32 +0000

    Creating a new "recommend" API within the OpenAIController,making use of PromptTemplate and specifying our own prompt to have control on the request and response structure

--- src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
+++ src/main/java/com/github/mourya/springailearning/controllers/OpenAIController.java
@@ -7,12 +7,14 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class OpenAIController {
@@ -56,5 +58,38 @@ public class OpenAIController {
                     .body("Error calling external OpenAI service.");
         }
 
+    }
+
+    @PostMapping("/openapi/recommend")
+    public ResponseEntity<String> recommend(@RequestParam String type,@RequestParam String year,
+                                            @RequestParam String lang) {
+
+        String template = """
+                           I want to watch a {type} movie released in {year} in {lang} language with a good rating
+                           Can you recommend me one movie along with cast,length of the movie and a brief description?
+
+                           Response should be in below format:
+                            Movie Name:
+                            Cast:
+                            Length:
+                            Imdb Rating:
+                            Description:
+                           """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+
+        Prompt prompt = promptTemplate.create(Map.of(
+                "type", type,
+                "year", year,
+                "lang", lang
+        ));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+
+        return ResponseEntity.ok(response);
     }
 }
