adcba47826661931d221bb60b2808df51aac7727
mouryachoudarybollineni <mouryachowdary38@gmail.com>
2025-12-10 13:15:18 +0000

    Cosine Similarity Implementation

--- src/main/java/com/github/mourya/springailearning/SpringAiLearningApplication.java
+++ src/main/java/com/github/mourya/springailearning/SpringAiLearningApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiLearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiLearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLearningApplication.class, args);
+    }
 
 }
